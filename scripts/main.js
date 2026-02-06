61382b6406bff359e6ee09bdbbf3ceb580471d2f
elcorremachitos <elcorremachitos.2015@gmail.com>
2026-02-05 21:59:59 -0500

    +modified getNumber() +added principalFunction()

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,17 @@
 console.log("Hello, World!");
 
-getNumber();
+principalFunction();
+
+function principalFunction() {
+    let option = getOperation();
+    let number1 = getNumber();
+    let number2 = getNumber();
+}
+
+function getOperation(){
+    return parseInt(prompt("1. Suma \n 2. Multiplicacion"));
+}
 
 function getNumber(){
-    return parseInt(prompt("1. Suma \n 2. Multiplicacion"));
+    return parseInt(prompt("Ingrese un numero entero"));
 }
