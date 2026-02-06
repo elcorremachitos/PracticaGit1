e92ae64727e6e5a45909cb5c0d59bbb9f6a9371f
elcorremachitos <elcorremachitos.2015@gmail.com>
2026-02-05 22:04:55 -0500

    +added calculateOperation() and showResult()

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,8 @@ function principalFunction() {
     let option = getOperation();
     let number1 = getNumber();
     let number2 = getNumber();
+
+    let result = calculateOperation(option,number1, number2);
 }
 
 function getOperation(){
@@ -14,4 +16,19 @@ function getOperation(){
 
 function getNumber(){
     return parseInt(prompt("Ingrese un numero entero"));
+}
+
+function calculateOperation(option,number1, number2){
+    switch(option){
+        case 1:
+            return number1+number2;
+        case 2:
+            return number1*number2;
+        default:
+            break;
+    }
+}
+
+function showResult(result){
+    alert("El resultado de la operacion es: \n${result}")
 }
